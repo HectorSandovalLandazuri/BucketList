729d72e2c2eed8f09cb49dbf1b67107ff577b75f
Héctor Sandoval <ingsan@live.com.mx>
2023-10-28 11:52:49 -0600

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Héctor Manuel Sandoval Landázuri on 28/10/23.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
